4095a43d3a0a74b9cde75e13a315070717feffbc
AbdulwahabAA297 <137956892+AbdulwahabAA297@users.noreply.github.com>
2023-07-28 09:37:03 +0300

    Update index.py

--- index.py
+++ index.py
@@ -1,68 +1,72 @@
+
 # Import the necessary modules
-import socket
-import threading
+
+import socketio
+
+import eventlet
+
 import cryptography.fernet
 
-# Create a socket object
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Create a socketio server object
 
-# Bind the socket to a port
-server.bind(('0.0.0.0', 8080))
+sio = socketio.Server()
 
-# Listen for incoming connections
-server.listen()
+# Create a web server object
+
+app = eventlet.wsgi.app
 
 # Generate a secret key for encryption and decryption
+
 key = cryptography.fernet.Fernet.generate_key()
+
 f = cryptography.fernet.Fernet(key)
 
-# A function to handle each client connection
-def handle_client(client):
-    # Receive the client's name
-    name = client.recv(1024).decode()
+# A function to handle the connection event
+
+@sio.event
+
+def connect(sid, environ):
+
     # Send the secret key to the client
-    client.send(key)
+
+    sio.emit('key', key, room=sid)
+
     # Print a message to the console
-    print(f'{name} has joined the chat.')
-    # Loop to receive and send messages
-    while True:
-        try:
-            # Receive an encrypted message from the client
-            message = client.recv(1024)
-            # Decrypt the message using the secret key
-            message = f.decrypt(message).decode()
-            # Print the message to the console
-            print(f'{name}: {message}')
-            # Broadcast the message to all other clients
-            broadcast(f'{name}: {message}'.encode())
-        except:
-            # Print a message to the console if an error occurs
-            print(f'{name} has left the chat.')
-            # Remove the client from the list of clients
-            clients.remove(client)
-            # Close the connection
-            client.close()
-            # Break out of the loop
-            break
 
-# A function to broadcast a message to all clients
-def broadcast(message):
-    # Encrypt the message using the secret key
-    message = f.encrypt(message)
-    # Loop through all clients in the list of clients
-    for client in clients:
-        # Send the encrypted message to each client
-        client.send(message)
+    print(f'Client {sid} has connected.')
 
-# A list to store all connected clients
-clients = []
+# A function to handle the message event
 
-# A loop to accept new connections
-while True:
-    # Accept a connection from a client
-    client, address = server.accept()
-    # Add the client to the list of clients
-    clients.append(client)
-    # Start a new thread to handle the client connection
-    thread = threading.Thread(target=handle_client, args=(client,))
-    thread.start()
+@sio.event
+
+def message(sid, data):
+
+    # Decrypt the message using the secret key
+
+    message = f.decrypt(data).decode()
+
+    # Print the message to the console
+
+    print(f'Client {sid}: {message}')
+
+    # Broadcast the message to all other clients
+
+    sio.emit('message', data, skip_sid=sid)
+
+# A function to handle the disconnect event
+
+@sio.event
+
+def disconnect(sid):
+
+    # Print a message to the console
+
+    print(f'Client {sid} has disconnected.')
+
+# Run the web server on port 8080
+
+if __name__ == '__main__':
+
+    print('Server is running on port 8080.')
+
+    eventlet.wsgi.server(eventlet.listen(('', 8080)), app)
